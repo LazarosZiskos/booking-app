a1c90076f140579943453f200ae6b8a74684ba69
ΛΑΖΑΡΟΣ ΖΗΣΚΟΣ <ziskoslaz@gmail.com>
2025-04-28 18:27:15 +0300

    project initialization

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+import { Inter, Open_Sans, Poppins } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "BookEase",
+  description: "Book your services with ease",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
